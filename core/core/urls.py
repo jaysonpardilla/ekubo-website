4de7ade1e964b17bbab6e06813bae9fa98f91eea
Jayson-prog <javerdomaraya7@gmail.com>
2026-01-08 00:02:47 +0800

    fdd

--- core/core/urls.py
+++ core/core/urls.py
@@ -5,10 +5,10 @@ from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('chat.urls')), 
-    path('products/', include('products.urls')),
-    path('manage-business/', include('manage_business.urls')),
-    path('shops/', include('shops.urls'))
+    path('', include('core.core.chat.urls')), 
+    path('products/', include('core.core.products.urls')),
+    path('manage-business/', include('core.core.manage_business.urls')),
+    path('shops/', include('core.core.shops.urls'))
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 
