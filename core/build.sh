df59ed75a152258b10f6dab62bd4696773381614
Jayson-prog <javerdomaraya7@gmail.com>
2026-01-07 22:47:55 +0800

    6

--- core/build.sh
+++ core/build.sh
@@ -2,10 +2,10 @@
 set -e
 
 echo "Installing dependencies..."
-pip install -r requirements.txt
+pip install -r core/requirements.txt
 
 echo "Running migrations..."
-python manage.py migrate --noinput
+cd core && python manage.py migrate --noinput
 
 echo "Collecting static files..."
 python manage.py collectstatic --noinput --clear
